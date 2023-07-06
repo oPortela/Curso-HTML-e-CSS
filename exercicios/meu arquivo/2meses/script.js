c35827f14196c8430ecc1e59d18832aaf788f133
Matheus Marques <matheusmarquesportela@gmail.com>
2023-07-06 16:13:12 -0300

    atualização

---
+++ exercicios/meu arquivo/2meses/script.js
@@ -0,0 +1,5 @@
+// Função para exibir uma mensagem de alerta quando a página for totalmente carregada
+window.onload = function() {
+    alert("Parabéns pelos dois meses de namoro! Que venham muitos mais meses felizes juntos!");
+  };
+  
